cb128969b3045f0be535b7fe0ef847d46e6c4d3e
IakMastro <cse242017102@uniwa.gr>
2020-03-13 17:51:04 +0200

    Client - Server - Database relationship #1

--- Client/client.py
+++ Client/client.py
@@ -6,14 +6,48 @@ from tkinter import messagebox
 HEADER_LENGTH = 10
 IP = '127.0.0.1'
 PORT = 8000
-message = "OK"
+username = "The Doctor"
 
+Ddef send_message(code):
+    message = f"{code}\n{name_entry.get()}\n1999/01/10\n2020/02/02"
+    message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+    client_socket.send(message_header + message.encode('utf-8'))
+
+def recieve_message():
+    try:
+        message_header = client_socket.recv(HEADER_LENGTH)
+
+        if not len(message_header):
+            return False
+        
+        message_length = int(message_header.decode('utf-8').strip())
+        return {'header': message_header, 'data': client_socket.recv(message_length)}
+    
+    except:
+        return False
+
+#Adding and deleting functions, accessed by the buttons
 def add_entry():
-    pass
+    send_message(1)
+    message = recieve_message()
+    message = message['data'].decode('utf-8')
+    if message == "OK":
+        messagebox.showinfo("Operation Successful!", "Person was successfully added to the database")
+
+    else:
+        messagebox.showwarning("Operation Failed.", "Person exists on the database already.")
 
 def delete_entry():
-    pass
+    send_message(2)
+    message = recieve_message()
+    message = message['data'].decode('utf-8')
+    if message == "OK":
+        messagebox.showinfo("Operation Successful!", "Person was successfully deleted from the database")
 
+    else:
+        messagebox.showwarning("Operation Failed.", "Person doesn't exist on the database.")
+
+#GUI Configuration
 top = Tk()
 top.title("Wikipedia Data Mining")
 name_frame = Frame(top)
@@ -36,8 +70,8 @@ try:
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((IP, PORT))
     client_socket.setblocking(False)
-    message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
-    client_socket.send(message_header + message.encode('utf-8'))
+    message_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
+    client_socket.send(message_header + username.encode('utf-8'))
 
 except:
     messagebox.showerror("No Connection to Server", "ERROR 404: Couldn't connect to the server. Press OK to exit.")
--- Server/server.py
+++ Server/server.py
@@ -27,9 +27,10 @@ server_socket.bind((IP, PORT))
 server_socket.listen()
 socket_list = [server_socket]
 clients = {}
-print(BLUE + f"Listening for connections on " + GREEN + "{IP}:{PORT}" + DEFAULT)
+print(BLUE + f"Listening for connections on " + GREEN + f"{IP}:{PORT}" + DEFAULT)
 
-def recieve_message(client_socket) -> bool:
+#Functions for recieving and sending messages
+def recieve_message(client_socket):
     try:
         message_header = client_socket.recv(HEADER_LENGTH)
 
@@ -42,6 +43,26 @@ def recieve_message(client_socket) -> bool:
     except:
         return False
 
+def send_message(client_socket, message):
+    message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+    client_socket.send(message_header + message.encode('utf-8'))
+
+#Function adding and deleting persons from the database
+def add_person(client_socket, name, birthday, deathday):
+    if db.insert(name, birthday, deathday):
+        send_message(client_socket, "OK")        
+
+    else:
+        send_message(client_socket, "ERROR: 402")
+
+def delete_person(client_address, name):
+    if db.delete(name):
+        send_message(client_socket, "OK")
+    
+    else:
+        send_message(client_socket, "ERROR: 402")
+
+#Server Configuration
 while True:
     read_sockets, _, exception_sockets = select.select(socket_list, [], socket_list)
 
@@ -55,8 +76,27 @@ while True:
 
             socket_list.append(client_socket)
             clients[client_socket] = user
-            print(BLUE + "Accepted new connection from" + GREEN +
+            print(BLUE + "Accepted new connection from " + GREEN +
              "{}:{}, message: {}".format(*client_address, user['data'].decode('utf-8')) + DEFAULT)
+
+        else:
+            message = recieve_message(notified_socket)
+
+            if message is False:
+                print(BLUE + f"Closed connection from: {clients[notified_socket]['data'].decode('utf-8')}" + DEFAULT)
+                socket_list.remove(notified_socket)
+                del clients[notified_socket]
+                continue
+
+            user = clients[notified_socket]
+            message = message["data"].decode("utf-8")
+            message = message.split('\n')
+
+            if message[0] is "1":
+                add_person(client_socket, message[1], message[2], message[3])
+
+            elif message[0] is "2":
+                delete_person(client_socket, message[1])
 
     for notified_socket in exception_sockets:
         socket_list.remove(notified_socket)
